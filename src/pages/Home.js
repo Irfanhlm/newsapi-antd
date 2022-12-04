691f61db7922b6476040915879d20a17f6febd6c
Irfan Halim <irfanhalimhidayat@gmail.com>
2022-12-04 14:56:38 +0700

    react-newsAPI-5

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,11 +9,11 @@ function Home() {
   const { Meta } = Card;
   const [data, setData] = useState([])
   const getNews = () => {
-    axios.get("https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=e937f314ff954947b479e4061975f904")
+    axios.get("https://newsapi.org/v2/everything?q=tesla&from=2022-11-04&sortBy=publishedAt&apiKey=e937f314ff954947b479e4061975f904")
       .then((response) => {
         setData(response.data.articles);
       })
-      .catch(error => console.error('Error:', error))
+      .catch((error) => console.log(error));
   }
 
 
