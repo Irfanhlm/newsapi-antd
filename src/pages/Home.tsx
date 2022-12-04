eba9f91a345e48be5184865dc7fb8b5c7f9b8582
Irfan Halim <irfanhalimhidayat@gmail.com>
2022-12-04 22:25:31 +0700

    react-newsAPI-7

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FC } from 'react';
+import React, { useState, useEffect, FC } from 'react';
 
 import { Button } from 'antd';
 import { Card } from 'antd';
@@ -9,46 +9,56 @@ import '../App.css';
 
 const Home: FC = () => {
   const { Meta } = Card;
+  const [loading, setLoading] = useState<Boolean>(true);
   const [data, setData] = useState<Array<Object>>([]);
+
+  useEffect(() => {
+    getNews();
+  }, [])
+
   const getNews = () => {
     axios.get("https://newsapi.org/v2/everything?q=tesla&from=2022-11-04&sortBy=publishedAt&apiKey=e937f314ff954947b479e4061975f904")
       .then((response) => {
         setData(response.data.articles);
         console.log(data)
       })
-      .catch((error) => console.log(error));
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
   }
 
-
-  return (
-    <>
-      <div className='btn'>
-        <button className='btn-btn' onClick={getNews}>Fecth News</button>
+  if (loading) {
+    return (
+      <div className='loader'>
+        <h2>Loading...</h2>
       </div>
-
-      <div className='container-main'>
-        <div className='container-card'>
-          {
-            data.map((value: any) => {
-              return (
-                <Card
-                  hoverable
-                  style={{
-                    width: '22rem'
-                  }}
-                  cover={<img alt="example" src={value.urlToImage} />}
-                >
-                  <Meta title={value.title} description={value.description} />
-                  <br></br>
-                  <Button type='primary' className='detail-url' href={value.url}>News Detail...</Button>
-                </Card>
-              )
-            })
-          }
+    )
+  } else {
+    return (
+      <>
+        <div className='container-main'>
+          <div className='container-card'>
+            {
+              data.map((value: any) => {
+                return (
+                  <Card
+                    hoverable
+                    style={{
+                      width: '22rem'
+                    }}
+                    cover={<img alt="example" src={value.urlToImage} />}
+                  >
+                    <Meta title={value.title} description={value.description} />
+                    <br></br>
+                    <Button type='primary' className='detail-url' href={value.url}>News Detail...</Button>
+                  </Card>
+                )
+              })
+            }
+          </div>
         </div>
-      </div>
-    </>
-  )
+      </>
+    )
+  }
 }
 
 export default Home;
